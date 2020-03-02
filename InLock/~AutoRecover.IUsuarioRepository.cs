c95c2d1ec98feaad55c53bd821912193330884c2
Matheus Esteves dos Santos <matheusesantos@gmail.com>
2020-03-02 11:25:00 -0300

    att

---
+++ InLock/~AutoRecover.IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using InLock.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InLock.Interfaces
+{
+    interface IUsuarioRepository 
+    {
+        List<UsuarioDomain> Listar();
+
+        void Cadastrar(UsuarioDomain cadastrarUsuario);
+
+        UsuarioDomain BuscarEmailSenha(string email, string senha);
+    }
+}
